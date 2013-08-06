3979ded2758ab9dcc649774b6baf51f0636c3e43
Chuck Preslar <chuckpreslar@gmail.com>
2013-08-06 11:59:41 -0400

    Fix tests after changing the definition of a task action.

--- gofer_test.go
+++ gofer_test.go
@@ -17,23 +17,14 @@ func TestRegister(t *testing.T) {
   Register(Task{
     Namespace: namespace,
     Label:     register,
-    Action: func() error {
+    Action: func(arguments ...string) error {
       fmt.Println("Register.")
-      return nil
-    },
-  }).Register(Task{
-    Namespace: namespace,
-    Label:     chaining,
-    Action: func() error {
-      fmt.Println("Chain register.")
       return nil
     },
   })
 
   if namespaceCount := len(gofer); 1 != namespaceCount {
     t.Errorf("Expected 1 namspace to be registered, got %d.", namespaceCount)
-  } else if taskCount := len(gofer[namespace]); 2 != taskCount {
-    t.Errorf("Expected 2 tasks to be registered, got %d.", taskCount)
   }
 }
 
@@ -43,7 +34,7 @@ func TestPreform(t *testing.T) {
   Register(Task{
     Namespace: namespace,
     Label:     preform,
-    Action: func() error {
+    Action: func(arguments ...string) error {
       unpreformed = !unpreformed
       return nil
     },
