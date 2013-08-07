7ddb71eeb69af14244e7a1250c5a33f2810cb1b7
Chuck Preslar <chuckpreslar@gmail.com>
2013-08-07 10:46:52 -0400

    Refactored task registration and preformance.

--- gofer.go
+++ gofer.go
@@ -2,222 +2,128 @@ package gofer
 
 import (
   "errors"
-  "fmt"
-  "io"
-  "os"
-  "path"
-  "runtime"
   "strings"
 )
 
-// Gofer package constants.
 const (
-  DELIMITER           = ":"     // Delimeter used to split string into namespace and task.
-  VERSION             = "0.0.1" // Gofer version constant.
-  PROTECTED_NAMESPACE = "tasks" // No namespace should be called `tasks`
-
-  LEFT_ALIGN = iota // Alignment for printing columns
-  RIGHT_ALIGN
+  DELIMITER = ":"
 )
 
-// Gofer error definitions.
 var (
-  ErrBadDef             = errors.New("Bad task definition.")                  // The task definition was malformed.
-  ErrNoNamesapce        = errors.New("Namespace is undefnied")                // The namespace was undefined.
-  ErrProtectedNamesapce = errors.New("Namespace is protected")                // The namespace used is `tasks`.
-  ErrNoTask             = errors.New("Task is undefined for namespace")       // The task was undefined.
-  ErrDefinedTask        = errors.New("Task is already defined for namespace") // The task was previously defined.
+  errBadLabel            = errors.New("Bad label for task, unexpected section delimiter.")
+  errRegistrationFailure = errors.New("Registration for task failed unexpectedly.")
+  errUnknownTask         = errors.New("Unable to look up task.")
+  errNoAction            = errors.New("No action defined for task.")
 )
 
-// Useful strings
-var (
-  GoPath        = os.Getenv("GOPATH")                // The GOPATH environtment variable.
-  SourcePrefix  = "/src/"                            // Go packages live in a `src` directory.
-  LabelColumn   = "            "                     // Label columns have a max length of 12.
-  PackageColumn = "                                " // Package columns have a max length of 32.
-)
+type action func(arguments ...interface{}) error
 
-// Action is a function that returns an error.
-type Action func(arguments ...string) error
-
-// Delegator is a used to map namespaces to tasks.
-// delegator[<namespace>][<task>] => Action
-type Delegator map[string]map[string]Task
-
-// Task defines an action to preform based on a namespace and label.
 type Task struct {
-  Namespace    string   // The namespace the task lives under.
-  Label        string   // The label the task is lives under.
-  Description  string   // The description of the task's action.
-  Dependencies []string // Slice of tasks that will be preformed prior to this tasks execution.
-  Action       Action   // The action preformed by executing the task.
-  pkg          string
+  Section      string
+  Label        string
+  Description  string
+  Dependencies []string
+  Action       action
+  manual       manual
+  location     string
 }
 
-var gofer = make(Delegator)
+type manual []*Task
 
-// Register accepts a Task, storing it for later usage
-// in an unexported Delegator.
-func Register(task Task) error {
-  var (
-    namespace = task.Namespace
-    label     = task.Label
-  )
+// gofer variable used for storing tasks.
+var gofer = make(manual, 0)
 
-  // A package attempted to register the PROTECTED_NAMESPACE of `tasks`
-  if PROTECTED_NAMESPACE == namespace {
-    return ErrProtectedNamesapce
-  }
+// index searches through the manual, returning a task
+// found with the label and in the section (namepsace) defined
+// by the definition.
+func (self *manual) index(definition string) (task *Task) {
+  sections := strings.Split(definition, DELIMITER)
+  entries := self
 
-  // Attach to directory of the caller to the package.
-  if _, file, _, ok := runtime.Caller(1); ok {
-    task.pkg = path.Dir(strings.TrimLeft(file, path.Join(GoPath, SourcePrefix)))
-  }
-
-  // If this is a new namespace, initialize it on the map.
-  if _, ok := gofer[namespace]; !ok {
-    gofer[namespace] = make(map[string]Task)
-  }
-
-  // The task has already been defined for the namespace.
-  if _, ok := gofer[namespace][label]; ok {
-    return ErrDefinedTask
-  }
-
-  gofer[namespace][label] = task
-
-  return nil
-}
-
-// Preform looks at the first string in the `arguments` given,
-// splitting it based on the constant `DELIMITER`,
-// calling the action defined.
-func Preform(arguments ...string) error {
-  definition := arguments[0]
-  split := strings.Split(definition, DELIMITER) // Split the difinition by the DELIMITER constant.
-
-  // Expect the split to result in an array with length of 2.
-  if 2 != len(split) {
-    return ErrBadDef
-  }
-
-  // Remove the `definition` argument from the array,
-  // the remainder will be passed to the action.
-  arguments = arguments[1:]
-
-  var namespace map[string]Task
-
-  //  Ensure the namespace exists.
-  if n, ok := gofer[split[0]]; ok {
-    namespace = n
-  } else {
-    return ErrNoNamesapce
-  }
-
-  task, ok := namespace[split[1]]
-
-  // Ensure the task lives in the namespace.
-  if !ok {
-    return ErrNoNamesapce
-  }
-
-  // If the task has any dependencies, preform them first.
-  for _, dep := range task.Dependencies {
-    if err := Preform(append([]string{dep}, arguments...)...); nil != err {
-      return err
-    }
-  }
-
-  return task.Action(arguments...)
-}
-
-// ListTasks prints a lists of the registered tasks to the writter.
-func ListTasks(writter io.Writer, arguments ...string) error {
-  // Generated template from binary provides `tasks` from the CLI
-  // as the first argument.
-  if PROTECTED_NAMESPACE == arguments[0] {
-    arguments = arguments[1:]
-  }
-
-  printTaskListHeader(writter)
-
-  // Assume all tasks are to be printed.
-  if 0 == len(arguments) {
-    return ListAllTasks(writter)
-  }
-
-  // Assume an only specific tasks are to be printed.
-  for _, namespace := range arguments {
-    if err := ListTasksFor(writter, namespace); nil != err {
-      return err
-    }
-  }
-
-  return nil
-}
-
-// ListAllTasks prints a list of all tasks to the writter.
-func ListAllTasks(writter io.Writer) error {
-  for namespace, _ := range gofer {
-    if err := ListTasksFor(writter, namespace); nil != err {
-      return err
-    }
-  }
-
-  return nil
-}
-
-// ListTasksFor prints a tasks for a specific namespace to the writter.
-func ListTasksFor(writter io.Writer, namespace string) error {
-  if n, ok := gofer[namespace]; ok {
-    index := 0
-
-    for _, t := range n {
-      var namespaceColumn string
-
-      // If this is the first iteration through the delegator for the task,
-      // print the namespace in the first column.
-      if index == 0 {
-        namespaceColumn = generateColumn(t.Namespace, LabelColumn, RIGHT_ALIGN)
-      } else {
-        namespaceColumn = LabelColumn
+  for _, section := range sections {
+    for i := 0; i < len(*entries); i++ {
+      if (*entries)[i].Label == section {
+        task = (*entries)[i]
+        entries = &task.manual // adjust `entries` pointer for next iteration.
+        break
       }
-
-      // Generate the other columns to be printed.
-      labelColumn := generateColumn(t.Label, LabelColumn, LEFT_ALIGN)
-      pkgColumn := generateColumn(t.pkg, PackageColumn, LEFT_ALIGN)
-      descriptionColumn := t.Description
-
-      fmt.Fprintf(writter, "%s %s %s %s\n", namespaceColumn, labelColumn, pkgColumn, descriptionColumn)
-      index += 1
     }
-  } else {
-    return ErrNoNamesapce
-  }
 
-  return nil
-}
-
-func printTaskListHeader(writter io.Writer) {
-  namespaceColumn := generateColumn("Namespace", LabelColumn, RIGHT_ALIGN)
-  labelColumn := generateColumn("Task", LabelColumn, LEFT_ALIGN)
-  pkgColumn := generateColumn("Directory", PackageColumn, LEFT_ALIGN)
-  descriptionColumn := "Description"
-  fmt.Fprintf(writter, "%s %s %s %s\n", namespaceColumn, labelColumn, pkgColumn, descriptionColumn)
-}
-
-// generateColumn is a helper for aligning output.
-func generateColumn(data, column string, alignment int) (result string) {
-  // If the data causes the column to overflow, truncate it.
-  if len(column) < len(data) {
-    data = fmt.Sprintf("...%s", data[len(data)-len(column)+3:])
-  }
-
-  if alignment == RIGHT_ALIGN {
-    result = fmt.Sprintf("%s%s", column[len(data):], data)
-  } else if alignment == LEFT_ALIGN {
-    result = fmt.Sprintf("%s%s", data, column[len(data):])
+    if nil == task {
+      return
+    } else if section != task.Label {
+      return nil
+    }
   }
 
   return
+}
+
+// sectionalize creates potential missing spaces in a manual
+// based on the `definition`.
+func (self *manual) sectionalize(definition string) (task *Task) {
+  task = self.index(definition)
+
+  if nil != task {
+    return
+  }
+
+  sections := strings.Split(definition, DELIMITER)
+
+  task = new(Task)
+  task.Label = sections[0]
+
+  *self = append(*self, task)
+
+  for i := 1; i < len(sections); i++ {
+    temp := new(Task)
+    temp.Section = strings.Join(sections[:i], DELIMITER)
+    temp.Label = sections[i]
+
+    task.manual = append(task.manual, temp)
+    task = temp // update task to temp for next iteration.
+  }
+
+  return
+}
+
+// Register accepts a `Task`, storing it for later.
+func Register(task Task) (err error) {
+  if index := strings.Index(task.Label, DELIMITER); -1 != index {
+    return errBadLabel
+  }
+
+  parent := gofer.sectionalize(task.Section)
+
+  if nil == parent {
+    if 0 != len(task.Section) {
+      return errRegistrationFailure
+    }
+
+    gofer = append(gofer, &task)
+  } else {
+    parent.manual = append(parent.manual, &task)
+  }
+
+  return
+}
+
+func Preform(definition string) (err error) {
+  task := gofer.index(definition)
+
+  if nil == task {
+    return errUnknownTask
+  } else if nil == task.Action {
+    return errNoAction
+  }
+
+  for _, dependency := range task.Dependencies {
+    err = Preform(dependency)
+
+    if nil != err {
+      return
+    }
+  }
+
+  return task.Action()
 }
--- gofer_test.go
+++ gofer_test.go
@@ -1,48 +1,118 @@
 package gofer
 
 import (
-  "fmt"
   "testing"
 )
 
-var (
-  namespace = "test"
-  register  = "register"
-  chaining  = "chaining"
-  preform   = "preform"
-)
+func TestManualIndex(t *testing.T) {
+  manual := manual([]*Task{&Task{Label: "one", manual: []*Task{&Task{Label: "two", manual: []*Task{&Task{Label: "three"}}}}}})
+  task := manual.index("one:two:three")
+
+  if nil == task || "three" != task.Label {
+    t.Errorf(`Expected label of nested tast to be "three", got %s.`, task)
+  }
+}
+
+func TestManualSectionalize(t *testing.T) {
+  manual := make(manual, 0)
+  manual.sectionalize("one:two:three")
+
+  task := manual.index("one:two:three")
+
+  if nil == task {
+    t.Error(`Unable to find task created during call to sectionalize.`)
+  } else if "one:two" != task.Section || "three" != task.Label {
+    t.Errorf(`Tasks was not created properly during call to sectionalize,`+
+      ` wanted "one:two" & "three", got "%s" & "%s".`, task.Section, task.Label)
+  }
+}
 
 func TestRegister(t *testing.T) {
+  task := Task{
+    Section: "one:two",
+    Label:   "three",
+  }
 
-  Register(Task{
-    Namespace: namespace,
-    Label:     register,
-    Action: func(arguments ...string) error {
-      fmt.Println("Register.")
-      return nil
-    },
-  })
+  Register(task)
 
-  if namespaceCount := len(gofer); 1 != namespaceCount {
-    t.Errorf("Expected 1 namspace to be registered, got %d.", namespaceCount)
+  stored := gofer.index("one:two:three")
+  parent := gofer.index("one:two")
+
+  if nil == stored || nil == parent {
+    t.Error(`Register failed to create and store task.`)
+  } else if "one:two" != stored.Section || "three" != stored.Label {
+    t.Errorf(`Register failed to store task properly, expected Section to be "one:two"`+
+      ` & Label to be "three", got %s & %s.`, stored.Section, stored.Label)
+  } else if "one" != parent.Section || "two" != parent.Label {
+    t.Errorf(`Register failed to create parent properly, expected Section to be "one:two"`+
+      ` & Label to be "three", got %s & %s.`, parent.Section, parent.Label)
+  }
+
+  other := Task{
+    Section: "one:two",
+    Label:   "four",
+  }
+
+  Register(other)
+  stored = gofer.index("one:two")
+
+  if 1 != len(gofer) {
+    t.Error(`Register failed to associate parent Section properly.`)
+  } else if 2 != len(stored.manual) {
+    t.Error(`Register failed to associate parent Section properly.`)
   }
 }
 
 func TestPreform(t *testing.T) {
-  unpreformed := true
+  unperformed := true
 
-  Register(Task{
-    Namespace: namespace,
-    Label:     preform,
-    Action: func(arguments ...string) error {
-      unpreformed = !unpreformed
+  task := Task{
+    Section: "one:two",
+    Label:   "five",
+    Action: func(arguments ...interface{}) error {
+      unperformed = false
       return nil
     },
-  })
+  }
 
-  Preform(fmt.Sprintf(`%s%s%s`, namespace, DELIMITER, preform))
+  Register(task)
+  err := Preform("one:two:five")
 
-  if unpreformed {
-    t.Error("Preform failed to flip boolean test flag.")
+  if nil != err {
+    t.Error(err)
+  } else if unperformed {
+    t.Error(`"unpreformed" flag was no flipped to false, call to Preform failed to run action.`)
+  }
+}
+
+func TestPreformWithDependencies(t *testing.T) {
+  unperformed := true
+
+  dependency := Task{
+    Section: "one:two",
+    Label:   "six",
+    Action: func(arguments ...interface{}) error {
+      unperformed = false
+      return nil
+    },
+  }
+
+  task := Task{
+    Section:      "one:two",
+    Label:        "seven",
+    Dependencies: []string{"one:two:six"},
+    Action: func(arguments ...interface{}) error {
+      return nil
+    },
+  }
+  Register(dependency)
+  Register(task)
+
+  err := Preform("one:two:seven")
+
+  if nil != err {
+    t.Error(err)
+  } else if unperformed {
+    t.Error(`"unpreformed" flag was no flipped to false, call to Preform failed to run dependency action.`)
   }
 }
