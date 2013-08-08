94ff083e7100b812cfa6a7757ffea1dd9b1103f8
Chuck Preslar <chuckpreslar@gmail.com>
2013-08-08 17:00:21 -0400

    Update gofer.rb

--- homebrew/gofer.rb
+++ homebrew/gofer.rb
@@ -1,15 +1,23 @@
 require 'formula'
 
 class Gofer < Formula
-  VERSION = "0.1.0"
+  VERSION = "0.0.3"
 
   homepage "https://github.com/chuckpreslar/gofer"
   head "https://github.com/chuckpreslar/gofer.git"
-  url ""
+  url "https://github.com/chuckpreslar/gofer/releases/download/v#{VERSION}/gofer_#{VERSION}.tar.gz"
   version VERSION
 
   def install
-    
+    unless system "echo $GOPATH"
+      abort "To use gofer, you must first set your $GOPATH environment variable set."     
+    end
+
+    unless system "go get -u github.com/chuckpreslar/gofer"
+      abort "Failed to install gofer package."
+    end
+
+    bin.install "gofer"
   end
 
 end
